36435c0b307c4690f46ad49d4a459d771bd8fa0f
Hemanth Kumar <hemanth20034@gmail.com>
2025-04-26 14:25:27 +0530

    build error commit

--- service/src/index.js
+++ service/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+require("dotenv").config();
 
 const authRoutes = require("./routes/auth-routes");
 const messageRoutes = require("./routes/message-routes");
@@ -10,7 +10,7 @@ const { dbConnect } = require("./lib/db");
 const { app, server, SocketServer } = require("./lib/socket");
 
 const PORT = process.env.PORT || 5000;
-const __dirnameBase = path.resolve();
+const __dirnameBase = path.resolve(__dirname, "..");
 
 SocketServer.initialize();
 
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
@@ -27,10 +27,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirnameBase, "../frontend/dist")));
+  app.use(express.static(path.join(__dirnameBase, "frontend", "dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirnameBase, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirnameBase, "frontend", "dist", "index.html"));
   });
 }
 
